2f021d118f88faa0398bd6b3da468cbac3c07f6d
aaajii <jwpbanawan@gmail.com>
2022-02-04 00:35:23 +0800

    Add getNFT function

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
-import twitterLogo from './assets/twitter-logo.svg';
+import { useState } from 'react'
+import { Connection, PublicKey } from '@solana/web3.js'
+import { bs58 } from '@project-serum/anchor/dist/cjs/utils/bytes'
 import './App.css';
-import { useState } from 'react';
-
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  const {inputValue, setInputValue}  = useState('')
+  const [inputValue, setAddressValue] = useState(null)
+  const metaDataKey = 4
+
+  const baseFilters = [
+    // Filter for MetadataV1 by key
+    {
+      memcmp: {
+        offset: 0,
+        bytes: bs58.encode(Buffer.from([metaDataKey])),
+      },
+    },
+  ].filter(Boolean)
+  const metaDataProgramID = 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s'
+  const updateAuthority = '53LS1oDAWmCMFaezgYqwprEwM1Ginrxj5SVDK6AaU7gB'
+  const clusterURL = 'https://nftarmory.genesysgo.net'
+
+  const getConnection = (committment) =>
+    new Connection(clusterURL, committment ?? 'processed')
+
+  const getNFTs = async (updateAuthority) => {
+    console.log('Pulling NFTs from:', updateAuthority)
+    const rawMetadatas = await getConnection().getProgramAccounts(
+      new PublicKey(metaDataProgramID),
+      {
+        filters: [
+          ...baseFilters,
+          {
+            memcmp: {
+              offset: 1,
+              bytes: bs58.encode(bs58.decode(updateAuthority)),
+            },
+          },
+        ],
+      }
+    )
+    console.log(rawMetadatas)
+  }
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
-          <p className="sub-text">
-            View your GIF collection in the metaverse ✨
-          </p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-
-          <input type="text" placeholder="Enter gif link!" 
-            value={inputValue}
-            onChange={({target}) => setInputValue(target.value)}/>
-          <button type="submit" className="cta-button submit-gif-button">Submit</button>
+          <input type="text" placeholder="enter address here" 
+          value={inputValue}
+          onChange={({target}) => setAddressValue(target.value)}/>
+          <button
+            type="submit"
+            className="cta-button submit-gif-button"
+            onClick={() => getNFTs(inputValue)}
+          >
+            Submit
+          </button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
